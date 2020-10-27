538faefe404614c046ec89fc952961b5232e40c6
isaacbrasil <isaac_brasil_oliveira@hotmail.com>
2020-10-26 22:43:57 -0300

    melhoras no  design

--- src/components/HomePage/index.js
+++ src/components/HomePage/index.js
@@ -1,26 +1,37 @@
 import React from 'react';
-import { Button, Form, FormGroup, Label, Input, FormText, Card, CardImg, CardText, CardBody,
-    CardTitle, CardSubtitle } from 'reactstrap';
+import {
+    Button, Form, FormGroup, Label, Input, FormText, Card, CardImg, CardText, CardBody,
+    CardTitle, CardSubtitle
+} from 'reactstrap';
 import "./styles.css";
 
 const Login = (props) => {
     return (
-        <div id="loginbox" >
-            <Form>
-                <FormGroup>
-                    <Label for="exampleEmail">Email</Label>
-                    <Input type="email" name="email" id="exampleEmail" placeholder="..." />
-                </FormGroup>
-                <FormGroup>
-                    <Label for="examplePassword">Password</Label>
-                    <Input type="password" name="password" id="examplePassword" placeholder="..." />
-                </FormGroup>
-            </Form>
-            <div id="btn-login" >
-                <Button color="success">login</Button>{' '}
-            </div>
+        <div id="wrap-loginbox" >
+
+            <div id="loginbox" >
+                <div id="register" >
+                    Autenticação
+                    </div >
+                <Form>
+                    <FormGroup>
+                        <Label for="exampleEmail">Email</Label>
+                        <Input type="email" name="email" id="exampleEmail" placeholder="email@email.com" />
+                    </FormGroup>
+                    <FormGroup>
+                        <Label for="examplePassword">Password</Label>
+                        <Input type="password" name="password" id="examplePassword" placeholder="password" />
+                    </FormGroup>
+                </Form>
+                <div id="btn-login" >
+                    <Button color="info">login</Button>{' '}
+                </div>
+            </div >
+
+
         </div >
-        
+
+
     );
 }
 export default Login;
